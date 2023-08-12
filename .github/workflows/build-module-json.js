45393468d4259a3e6bfd96855c429a9e13f246f3
Ernest Scott Skousen <ernest@division4studios.com>
2023-08-12 14:13:05 -0400

    Update Name of Module
    Update Name of Module

--- .github/workflows/build-module-json.js
+++ .github/workflows/build-module-json.js
@@ -5,6 +5,6 @@ const version = JSON.parse(fs.readFileSync('package.json', 'utf8')).version;
 var mod = JSON.parse(fs.readFileSync('module-template.json', 'utf8'));
 
 mod.version = version;
-mod.download = `https://github.com/mrprimate/ddb-importer/releases/download/${version}/ddb-importer.zip`;
+mod.download = `https://github.com/Division4Studios/div4io-procedural-shops-and-loot/releases/download/${version}/div4io-procedural-shops-and-loot.zip`;
 
 console.log(JSON.stringify(mod));
