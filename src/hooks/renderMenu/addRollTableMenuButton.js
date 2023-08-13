f79e32f4d565c6dbb10454d7bbaeb7b679c9f349
Ernest Scott Skousen <ernest@division4studios.com>
2023-08-13 15:21:16 -0400

    Add Button For Roll Table Menu Popup
    Add Button For Roll Table Menu Popup

--- src/hooks/renderMenu/addRollTableMenuButton.js
+++ src/hooks/renderMenu/addRollTableMenuButton.js
@@ -1,5 +1,5 @@
 export function addRollTableMenuButton(app, html) {
-  if (app.options.id == "tables" && game.user.isGM) {
+  if (app.options.classes.includes("tables-sidebar") && game.user.isGM) {
     let button = $("<div class='header-actions action-buttons flexrow'><button class='ddb-muncher'><i class='fas fa-th-list'></i> Generate Tables </button></div>");
 
     $(html).find(".directory-header").append(button);
